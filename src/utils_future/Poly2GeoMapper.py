1d7232df2c305f2b3b0b8ed4c3e4d97183e4f79b
nuuuwan <nuuuwan@gmail.com>
2025-12-05 13:26:02 +0530

    Add Poly2GeoMapper class for coordinate transformation and fitting

---
+++ src/utils_future/Poly2GeoMapper.py
@@ -0,0 +1,43 @@
+import numpy as np
+from utils import Log
+
+log = Log("Poly2GeoMapper")
+
+
+class Poly2GeoMapper:
+
+    @staticmethod
+    def fit(xys: list[tuple[int, int]], latlngs: list[tuple[float, float]]):
+        assert len(xys) == len(
+            latlngs
+        ), "Length of xys and latlngs must be equal"
+        xys = np.asarray(xys)
+        xs = xys[:, 0]
+        ys = xys[:, 1]
+        lats = np.asarray([latlng[0] for latlng in latlngs])
+        lngs = np.asarray([latlng[1] for latlng in latlngs])
+
+        A = np.column_stack([xs**2, ys**2, xs * ys, xs, ys, np.ones_like(xs)])
+
+        lat_params, *_ = np.linalg.lstsq(A, lats, rcond=None)
+        lng_params, *_ = np.linalg.lstsq(A, lngs, rcond=None)
+
+        log.debug(f"{lat_params=}, {lng_params=}")
+        return lat_params, lng_params
+
+    @staticmethod
+    def transform(xy: tuple, lat_params, lng_params):
+        x, y = xy
+        x = np.asarray(x)
+        y = np.asarray(y)
+
+        # Ensure A has shape (N, 6)
+        A = np.column_stack([x**2, y**2, x * y, x, y, np.ones_like(x)])
+
+        lat = A.dot(lat_params)
+        lng = A.dot(lng_params)
+
+        # Return scalars if input was scalar
+        if lat.size == 1:
+            return float(lat), float(lng)
+        return (lat, lng)
---
+++ src/utils_future/__init__.py
@@ -0,0 +1,4 @@
+# utils_future (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from utils_future.Poly2GeoMapper import Poly2GeoMapper
